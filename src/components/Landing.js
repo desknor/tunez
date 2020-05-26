af4a9f9c1713adbccddf3d46abb284081c377d2b
desknor <deschdev@gmail.com>
2020-05-26 17:44:01 -0400

    landing page complete

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 const Landing = () => (
     <section className="landing">
-        Landing page will go here
+        <h1 className="hero-title">Turn that music up!</h1>
+    <section className="selling-points">
+        <div className="point">
+            <h2 className="point-title">Choose Your Music</h2>
+            <p className="point-description">The world is full of music; why listen to music that someone else chose?!</p>
+        </div>
+        <div className="point">
+            <h2 className="point-title">Unlimited, streaming, ad-free</h2>
+            <p className="point-description">No arbitrary limits. No distractions.</p>
+        </div>
+        <div className="point">
+            <h2 className="point-title">Mobile Enabled</h2>
+            <p className="point-description">Listen to your music on the go.</p>
+        </div>
+    </section>
     </section>
 );
 
